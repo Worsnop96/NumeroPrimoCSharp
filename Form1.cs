37803d26ca16a24e88b85b7e89750368d07a7ced
Dylan Estrada <dylan@hotmail.com>
2021-10-22 18:15:19 -0500

    Primer commit practica numero primo con c sharp

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroPrimo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int numero = Int32.Parse(txtIngreso.Text);
+
+
+
+
+            if (checkPrimo(numero) == false)
+            {
+                MessageBox.Show("Numero ingresado no es primo");
+            }
+            else
+            {
+                MessageBox.Show("Numero ingresado es primo");
+            }
+
+
+        }
+
+
+        private Boolean checkPrimo(int numero)
+        {
+            Boolean isPrimo = true;
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    isPrimo = false;
+                }
+            }
+
+            return isPrimo;
+        }
+
+    }
+}
